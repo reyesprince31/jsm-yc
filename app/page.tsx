f83360c4b7b4fe4530f4ab8be2e8c38a0ab26e13
reyesprince31 <reyes.prince31@gmail.com>
2024-10-26 17:38:54 +0800

    cleanup initial setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,7 @@
 export default async function Home() {
-  const response = await fetch("https://jsonplaceholder.typicode.com/albums");
-
-  if (!response.ok) {
-    throw new Error("Failed to fetch data");
-  }
-
-  const albums = await response.json();
-
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-      {albums.map((album: { id: number; title: string }) => (
-        <div key={album.id} className="bg-white shadow-md rounded-lg p-4">
-          <h2 className="text-xl font-bold mb-2">{album.title}</h2>
-          <p className="text-gray-600">Album ID: {album.id}</p>
-        </div>
-      ))}
+    <div>
+      <h1>Hello</h1>
     </div>
   );
 }
--- components/hello.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-function Hello() {
-  console.log("hello from client component");
-  return <div>Hello</div>;
-}
-
-export default Hello;

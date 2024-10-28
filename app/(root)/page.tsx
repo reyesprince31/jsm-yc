36555a35a3f13ab5f6acbcb414548b19c849f4e2
reyesprince31 <reyes.prince31@gmail.com>
2024-10-28 15:43:53 +0800

    feature startup card with mock data

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import SearchForm from "./_components/search-form";
+import StartupCard from "./_components/startup-card";
 
 export default async function Home({
   searchParams,
@@ -6,6 +7,23 @@ export default async function Home({
   searchParams: Promise<{ query?: string }>;
 }) {
   const query = (await searchParams).query;
+
+  const posts = [
+    {
+      _id: "1",
+      title: "Startup 1",
+      category: "Technology",
+      image: "https://vichinth.com/wp-content/uploads/2018/07/startup-2.jpg",
+      author: {
+        name: "Author 1",
+        _id: "1",
+        image: "https://reqres.in/img/faces/1-image.jpg",
+      },
+      description: "This is the description of the startup 1",
+      views: 100,
+      _createdAt: "2022-01-01",
+    },
+  ];
 
   return (
     <div>
@@ -20,6 +38,22 @@ export default async function Home({
         </p>
 
         <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All Startups"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupTypeCard) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No startups found</p>
+          )}
+        </ul>
       </section>
     </div>
   );

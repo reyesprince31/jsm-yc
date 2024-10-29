d373858e5403dfb13cd9a2b66f432ff8f292e170
reyesprince31 <77110899+reyesprince31@users.noreply.github.com>
2024-10-29 21:06:58 +0800

    feature writeClient for views on every render of page details

--- app/(root)/_components/view.tsx
+++ app/(root)/_components/view.tsx
@@ -2,10 +2,22 @@ import { client } from "@/sanity/lib/client";
 import { Ping } from "./ping";
 import { STARTUP_VIEWS_QUERY } from "@/sanity/lib/queries";
 import { use } from "react";
+import { writeClient } from "@/sanity/lib/write-client";
+import { unstable_after as after } from "next/server";
 
 export function View({ id }: { id: string }) {
   const { views: totalViews } = use(
     client.withConfig({ useCdn: false }).fetch(STARTUP_VIEWS_QUERY, { id })
+  );
+
+  after(
+    async () =>
+      await writeClient
+        .patch(id)
+        .set({
+          views: totalViews + 1,
+        })
+        .commit()
   );
 
   return (
